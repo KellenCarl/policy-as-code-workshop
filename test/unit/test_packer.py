8c1c70e33bc93ee80787d5860a3e862b24c83751
Kellen Carl <67493450+KellenCarl@users.noreply.github.com>
2021-05-23 15:03:15 -0500

    Update unit tests to pass

--- test/unit/test_packer.py
+++ test/unit/test_packer.py
@@ -12,12 +12,14 @@ def image_config():
 # Tests the container source image for Ubuntu
 # It's ok if your function is a little longer so
 # someone can understand your intent!
-def test_container_source_image_for_ubuntu_base(image_config):
-    assert False
+def test_packer_configuration_for_ubuntu_container_source_image(image_config):
+    base_image = image_config['source'][0]['docker']['pac']['image'][0]
+    assert base_image == 'ubuntu'
 
 
 # Tests that fake-service uses a specific version.
 # Good to include if you want to be declarative about
 # potentially patched images!
 def test_shell_provisioner_for_fake_service_version(image_config):
-    assert False
+    fake_service_version = image_config['build'][0]['provisioner'][0]['shell']['environment_vars'][0][0]
+    assert '0.21.0' in fake_service_version
