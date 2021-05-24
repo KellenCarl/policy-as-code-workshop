e89c6444fc873fab23e1cc938a6c1f8cbc06344f
Kellen Carl <67493450+KellenCarl@users.noreply.github.com>
2021-05-23 21:53:38 -0500

    Add Policy as code to unit test. No root user. User should be ubuntu.

--- test/integration/controls/fake_service.rb
+++ test/integration/controls/fake_service.rb
@@ -5,13 +5,16 @@ title "security and policy for fake-service"
 # Check if the fake-service binary exists
 # and its owner is not root.
 describe file("/usr/local/bin/fake-service") do
-  it { should_not exist }
+  it { should exist }
+  its('owner') { should_not eq 'root' }
 end
 
 # Ensure that the ubuntu user exists
 # in the container
 describe user('ubuntu') do
   it { should exist }
+  its('groups') { should eq ['ubuntu'] }
+  its('shell') { should eq '/bin/sh' }
 end
 
 
